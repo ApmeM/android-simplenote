74d07a9f7f6b5c68e7089ef42d22485104e68d5e
Artem Votincev <artem.votincev@gmail.com>
2011-10-04 14:43:46 +0400

    [CHG] move delete button into edit element

--- src/main/java/org/apmem/widget/notes/SimpleNoteWidgetItemActivity.java
+++ src/main/java/org/apmem/widget/notes/SimpleNoteWidgetItemActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import org.apmem.widget.notes.datastore.ListsItemRepository;
 import org.apmem.widget.notes.datastore.ListsWidgetRepository;
@@ -23,8 +24,8 @@ import org.apmem.widget.notes.refresh.impl.RefresherFromActivity;
  * Time: 0:24
  * To change this template use File | Settings | File Templates.
  */
-public class SimpleNoteWidgetItemAddActivity extends Activity {
-    private static final String TAG = "SimpleNoteWidgetItemAddActivity";
+public class SimpleNoteWidgetItemActivity extends Activity {
+    private static final String TAG = "SimpleNoteWidgetItemActivity";
 
     private ListsWidgetRepository listsWidgetRepository = new ListsWidgetRepositoryFake();
     private ListsItemRepository listsItemRepository = new ListsItemRepositoryFake();
@@ -34,16 +35,19 @@ public class SimpleNoteWidgetItemAddActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_add);
+        setContentView(R.layout.activity_item);
 
-        EditText editText = (EditText) this.findViewById(R.id.activity_item_add_text);
+        EditText editText = (EditText) this.findViewById(R.id.activity_item_text);
+        Button deleteButton = (Button) this.findViewById(R.id.activity_item_button_delete);
 
         long itemId = this.getIntent().getLongExtra(Constants.INTENT_EXTRA_WIDGET_ITEM_ID, -1l);
         ListItemElement item = this.listsItemRepository.get(itemId);
         if (item != null) {
             editText.setText(item.getName());
+            deleteButton.setVisibility(View.VISIBLE);
         } else {
             editText.setText("");
+            deleteButton.setVisibility(View.INVISIBLE);
         }
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
@@ -51,7 +55,7 @@ public class SimpleNoteWidgetItemAddActivity extends Activity {
     public void onCommit(View button) {
         Log.i(TAG, "onCommit");
 
-        EditText editText = (EditText) this.findViewById(R.id.activity_item_add_text);
+        EditText editText = (EditText) this.findViewById(R.id.activity_item_text);
         String name = editText.getText().toString();
 
         if (!name.trim().equals("")) {
@@ -73,6 +77,21 @@ public class SimpleNoteWidgetItemAddActivity extends Activity {
 
     public void onCancel(View button) {
         Log.i(TAG, "onCancel");
+        this.finish();
+    }
+
+
+    public void onDelete(View button) {
+        Log.i(TAG, "onDelete");
+
+        long itemId = this.getIntent().getLongExtra(Constants.INTENT_EXTRA_WIDGET_ITEM_ID, -1l);
+        int appWidgetId = this.getIntent().getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 0);
+        ListWidgetElement widget = this.listsWidgetRepository.get(appWidgetId);
+
+        this.listsItemRepository.remove(itemId);
+
+        this.refresher.updateList(this, widget.getListId());
+
         this.finish();
     }
 }
--- src/main/java/org/apmem/widget/notes/SimpleNoteWidgetProvider.java
+++ src/main/java/org/apmem/widget/notes/SimpleNoteWidgetProvider.java
@@ -87,15 +87,6 @@ public class SimpleNoteWidgetProvider extends AppWidgetProvider {
                 remoteViews.setViewVisibility(R.id.widget_layout_button_add, View.GONE);
             }
             this.updateWidget(context, appWidgetId, remoteViews);
-        } else if (action.equals(Constants.ACTION_WIDGET_UPDATE_FROM_WIDGET_DELETE_ITEM)) {
-            int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID);
-            long itemId = extras.getLong(Constants.INTENT_EXTRA_WIDGET_ITEM_ID);
-            ListItemElement item = this.listsItemRepository.get(itemId);
-            if (item != null) {
-                this.listsItemRepository.remove(itemId);
-                this.refresher.updateList(context, item.getListId());
-            }
-            this.updateWidget(context, appWidgetId, remoteViews);
         } else if (action.equals(Constants.ACTION_WIDGET_UPDATE_FROM_WIDGET_READY_ITEM)) {
             int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID);
             long itemId = extras.getLong(Constants.INTENT_EXTRA_WIDGET_ITEM_ID);
@@ -122,11 +113,10 @@ public class SimpleNoteWidgetProvider extends AppWidgetProvider {
             text = stringUnderline;
         }
 
-        RemoteViews newView = new RemoteViews(remoteViews.getPackage(), R.layout.widget_layout_row);
+        RemoteViews newView = new RemoteViews(remoteViews.getPackage(), R.layout.widget_layout_row_2_2);
         newView.setTextViewText(R.id.widget_layout_row_text, text);
         remoteViews.addView(R.id.widget_layout_list, newView);
         this.setOnEditItemClick(context, item.getId(), appWidgetId, newView);
-        this.setOnDeleteItemClick(context, item.getId(), appWidgetId, newView);
         this.setOnReadyItemClick(context, item.getId(), appWidgetId, newView);
     }
 
@@ -136,7 +126,7 @@ public class SimpleNoteWidgetProvider extends AppWidgetProvider {
     }
 
     private void setOnEditItemClick(Context context, long itemId, int appWidgetId, RemoteViews remoteViews) {
-        Intent newIntent = new Intent(context, SimpleNoteWidgetItemAddActivity.class);
+        Intent newIntent = new Intent(context, SimpleNoteWidgetItemActivity.class);
         newIntent.putExtra(Constants.INTENT_EXTRA_WIDGET_ITEM_ID, itemId);
         newIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 
@@ -146,34 +136,6 @@ public class SimpleNoteWidgetProvider extends AppWidgetProvider {
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         remoteViews.setOnClickPendingIntent(R.id.widget_layout_row_button_edit, pendingIntent);
-    }
-
-    private void setOnDeleteItemClick(Context context, long itemId, int appWidgetId, RemoteViews remoteViews) {
-        // Check settings:
-        boolean showDialog = true; // ToDo: some settings.
-
-        Intent newIntent;
-        if (showDialog) {
-            newIntent = new Intent(context, SimpleNoteWidgetItemDeleteActivity.class);
-        } else {
-            newIntent = new Intent(context, SimpleNoteWidgetProvider.class);
-            newIntent.setAction(Constants.ACTION_WIDGET_UPDATE_FROM_WIDGET_DELETE_ITEM);
-        }
-        newIntent.putExtra(Constants.INTENT_EXTRA_WIDGET_ITEM_ID, itemId);
-        newIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-
-        // When intents are compared, the extras are ignored, so we need to embed the extras
-        // into the data so that the extras will not be ignored.
-        newIntent.setData(Uri.parse(newIntent.toUri(Intent.URI_INTENT_SCHEME)));
-
-        PendingIntent pendingIntent;
-        if (showDialog) {
-            pendingIntent = PendingIntent.getActivity(context, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        } else {
-            pendingIntent = PendingIntent.getBroadcast(context, 0, newIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        }
-
-        remoteViews.setOnClickPendingIntent(R.id.widget_layout_row_button_remove, pendingIntent);
     }
 
     private void setOnReadyItemClick(Context context, long itemId, int appWidgetId, RemoteViews remoteViews) {
@@ -195,7 +157,7 @@ public class SimpleNoteWidgetProvider extends AppWidgetProvider {
     }
 
     private void setOnAddItemClick(Context context, int appWidgetId, RemoteViews remoteViews) {
-        Intent newIntent = new Intent(context, SimpleNoteWidgetItemAddActivity.class);
+        Intent newIntent = new Intent(context, SimpleNoteWidgetItemActivity.class);
         newIntent.putExtra(Constants.INTENT_EXTRA_WIDGET_ITEM_ID, -1l);
         newIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
 
